667f22108bce60c043eb0a3206be2512f123f9a9
themlprofessional <117509273+themlprofessional@users.noreply.github.com>
2023-04-02 10:51:32 +0530

    Delete uber directory

--- uber/final_csv_file/sql_query.sql
+++
@@ -1,9 +0,0 @@
---left join of weather table on rides table
-
-select * from dbo.addresses -- rides table
-select * from dbo.joined_weather_data -- weather table
-
-SELECT *
-FROM dbo.addresses a
-LEFT JOIN dbo.joined_weather_data b
-ON CONVERT(DATE, a.date_time) = b.datetime
